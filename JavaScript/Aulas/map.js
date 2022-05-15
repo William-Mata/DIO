67b215100d6c8ce3b0dde037c1b0229ba625554f
William-Mata <william.xavante@gmail.com>
2022-05-15 17:23:53 -0300

    Organizando

--- JavaScript/Aulas/map.js
+++
@@ -1,9 +0,0 @@
-const valores = [5,1,4,6,7,8];
-
-const mult = {value: 2,}
-
-console.log(valores.map(function (val) {
-    return val * this.value;
- }, mult));
-
-console.log(valores.map((num) => num * num));
--- JavaScript/filter.js
+++
@@ -1,4 +0,0 @@
-const valores = [1,2,3,4,5,6,7,8,9,10];
-
-
-console.log(valores.filter((valor) => valor % 2 === 0))
